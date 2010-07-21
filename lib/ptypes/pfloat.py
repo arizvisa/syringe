414e6deb4041941cf8ab6a836961d09728efc648
arizvisa@local <arizvisa@gmail.com>
2010-07-21 00:25:46 -0500

    Finished the single/double definition types. Added some tests too

--- lib/ptypes/pfloat.py
+++ lib/ptypes/pfloat.py
@@ -7,8 +7,9 @@ class float_t(pint.integer_t):
         mask = (1<<bits) - 1
         return (int>>bits, int&mask)
 
-    def __getcomponents(self, integer):
+    def getcomponents(self, integer):
         sign,exponent,fraction = self.components
+        res = integer
         res,fraction = self.__consume_bits(res, fraction)
         res,exponent = self.__consume_bits(res, exponent)
         res,sign = self.__consume_bits(res, sign)
@@ -19,16 +20,22 @@ class float_t(pint.integer_t):
         bitmask = (1<<bits) - 1
 
         res = int(self)
-        sign,exponent,fraction = self.__getcomponents(res)
-        exponentshift = (2**self.components[1]) / 2 - 1
+        sign,exponent,fraction = self.getcomponents(res)
+        exponentshift = (2*self.components[1]) / 2 - 1
         fractionscale = 2**self.components[2]
 
         # convert to float()
-        sign = -1**sign
-        exponent = 2**exponentshift
-        fraction = 1.0 + (1.0/fractionscale)
+        negative = -1*sign
+        e = exponent - (2**exponentshift-1)
+        mantissa = 1.0+(fraction*(1.0/fractionscale))
+        
+        # and now we stack up imprecision
+        if sign:
+            return -1 * mantissa * (2**e)
+        return mantissa * (2**e)
 
-        raise NotImplementedError( repr((sign,exponent,fraction)) )
+    def __repr__(self):
+        return '%s %s (%x)'% (self.name(), float(self), int(self))
 
 if False:
     class half(float_t):
@@ -42,3 +49,45 @@ class single(float_t):
 class double(float_t):
     length = 8
     components = (1, 11, 52)
+
+if __name__ == '__main__':
+
+    def test(integer, f1, f2):
+        if f1 == f2:
+            print "SUCCESS : %x : %s == %s"%( integer, repr(f1), repr(f2))
+            return
+        print "FAIL : %x : %s == %s"%( integer, repr(f1), repr(f2))
+
+    singles = [
+        (0x3f800000, 1.0),
+        (0xc0000000, -2.0),
+        (0x7f7fffff, 3.4028234663852886e+38),
+        #        (0x00000000, 0.0),
+        #        (0x80000000, -0.0),
+        #        (0x7f800000, 0),
+        #        (0xff800000, -0),
+        (0x3eaaaaaa, 1.0/3),
+        (0x41c80000, 25.0),
+    ]
+
+    self = single()
+    for number,result in singles:
+        self.set(number)
+        test( number, float(self), result )
+
+
+    ######3#
+    doubles = [
+        (0x3ff0000000000000, 1.0),
+        (0x3ff0000000000001, 1.0000000000000002),
+        (0x3ff0000000000002, 1.0000000000000004),
+        (0x4000000000000000, 2.0),
+        (0xc000000000000000, -2.0),
+        (0x3fd5555555555555, 0.3333333333333333),
+    ]
+
+    self = double()
+    for number,result in doubles:
+        self.set(number)
+        test( number, float(self), result )
+    pass
