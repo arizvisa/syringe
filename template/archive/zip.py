20d5a34f353b0b455d76b46d995ebd4b3abf3674
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-06-14 16:49:36 -0500

    Some logging improvements to the template/archive.zip tool.
    template/archive.zip:
        Replaced the verbose option with a new option "--list-all" which
            lists the entire zip directory instead of just files.
        Fixed the default logging level to output any warnings or
            any critical errors that occur.
        Added a verbosity option that results in archive.zip emitting
            more detailed information about what's going on.
        Added a call to logging.info that outputs the number of bytes
            that get written.
        Fixed some comments spacing issues that result in easier reading.

--- template/archive/zip.py
+++ template/archive/zip.py
@@ -1,5 +1,5 @@
 import logging
-logging.root.setLevel(100)
+logging.root.setLevel(logging.WARNING)
 
 import ptypes
 from ptypes import *
@@ -9,7 +9,7 @@ class MSTime(pint.uint16_t): pass
 class MSDate(pint.uint16_t): pass
 
 class VersionMadeBy(pint.enum, pint.uint16_t):
-    _values_ = [(n,i) for i,n in enumerate(('MSDOS', 'Amiga', 'OpenVMS', 'Unix', 'VM/CMS', 'Atari ST', 'OS/2', 'Macintosh', 'Z-System', 'CP/M', 'Windows', 'MVS', 'VSE', 'Acorn', 'VFAT', 'Alternate MVS', 'BeOS', 'Tandem', 'Os/400', 'OSX'))]
+    _values_ = [(n, i) for i, n in enumerate(('MSDOS', 'Amiga', 'OpenVMS', 'Unix', 'VM/CMS', 'Atari ST', 'OS/2', 'Macintosh', 'Z-System', 'CP/M', 'Windows', 'MVS', 'VSE', 'Acorn', 'VFAT', 'Alternate MVS', 'BeOS', 'Tandem', 'Os/400', 'OSX'))]
 class VersionNeeded(pint.enum, pint.uint16_t):
     _values_ = []
 
@@ -64,20 +64,20 @@ class CompressionMethod(pint.enum, pint.uint16_t):
 class CompressionMethodFlags(ptype.definition):
     cache = {}
     class unknown(pbinary.struct):
-        _fields_ = [(2,'unknown')]
+        _fields_ = [(2, 'unknown')]
 
 @CompressionMethodFlags.define(type=6)
 class MethodImplodingFlags(pbinary.flags):
-    _fields_ = [(1,'8kDictionary'),(1,'3Shannon-FanoTrees')]
+    _fields_ = [(1, '8kDictionary'), (1, '3Shannon-FanoTrees')]
 
 @CompressionMethodFlags.define(type=8)
 @CompressionMethodFlags.define(type=9)
 class MethodDeflatingFlags(pbinary.struct):
-    _fields_ = [(2,'Quality')]
+    _fields_ = [(2, 'Quality')]
 
 @CompressionMethodFlags.define(type=14)
 class MethodLZMAFlags(pbinary.flags):
-    _fields_ = [(1,'EOS'),(1,'unused')]
+    _fields_ = [(1, 'EOS'), (1, 'unused')]
 
 ## Extra data field mappings
 
@@ -94,7 +94,7 @@ class ZipRecord(ptype.definition):
     cache = {}
     attribute = 'signature'
 
-@ZipRecord.define(signature=(32,0x04034b50))
+@ZipRecord.define(signature=(32, 0x04034b50))
 class LocalFileHeader(pstruct.type):
     _fields_ = [
         (pint.uint16_t, 'version needed to extract'),
@@ -117,7 +117,7 @@ class LocalFileHeader(pstruct.type):
         if not kwds.get('decompress', False):
             return self['file data'].serialize()
 
-        res,method = self['file data'].serialize(),self['compression method']
+        res, method = self['file data'].serialize(), self['compression method']
         if method['Stored']:
             return res
         elif method['Deflated']:
@@ -136,11 +136,11 @@ class LocalFileHeader(pstruct.type):
         cls = self.classname()
         meth = self['compression method'].str()
         descr = self['data descriptor'].summary()
-        time,date = self['last mod file time'],self['last mod file date']
+        time, date = self['last mod file time'], self['last mod file date']
         filename = self['file name'].str()
         return '{:s}) {:s} {!r} method={:s} {:s} time/date={:04x}:{:04x}'.format(index, cls, filename, meth, descr, time.int(), date.int())
 
-@ZipRecord.define(signature=(32,0x02014b50))
+@ZipRecord.define(signature=(32, 0x02014b50))
 class CentralDirectory(pstruct.type):
     _fields_ = [
         (VersionMadeBy, 'version made by'),
@@ -170,11 +170,11 @@ class CentralDirectory(pstruct.type):
         cls = self.classname()
         meth = self['compression method'].str()
         descr = self['data descriptor'].summary()
-        time,date = self['last mod file time'],self['last mod file date']
+        time, date = self['last mod file time'], self['last mod file date']
         filename = self['file name'].str()
         return '{:s}) {:s} {!r} version-made-by={:s} version-needed-to-extract={:s} compression-method={:s} {:s} last-mod-file-time/date={:04x}:{:04x} disk-number-start={:d} internal-file-attributes=0x{:x} external-file-attributes=0x{:x}'.format(index, cls, filename, self['version made by'].str(), self['version needed to extract'].str(), meth, descr, time.int(), date.int(), self['disk number start'].int(), self['internal file attributes'].int(), self['external file attributes'].int()) + ("// %s"%self['file comment'].str() if self['file comment length'].int() > 0 else '')
 
-@ZipRecord.define(signature=(32,0x06054b50))
+@ZipRecord.define(signature=(32, 0x06054b50))
 class EndOfCentralDirectory(pstruct.type):
     _fields_ = [
         (pint.uint16_t, 'number of this disk'),
@@ -195,7 +195,7 @@ class EndOfCentralDirectory(pstruct.type):
         cls = self.classname()
         return '{:s}) {:s} number-of-this-disk={:d} number-of-this-disk-with-the-start-of-the-central-directory={:d} total-number-of-entries-in-the-central-directory={:d} size-of-the-central-directory=0x{:x} offset-of-the-start-of-central-directory-with-respect-to-the-starting-disk-number={:d}'.format(index, cls, self['number of this disk'].int(), self['number of the disk with the start of the central directory'].int(), self['total number of entries in the central directory on this disk'].int(), self['total number of entries in the central directory'].int(), self['size of the central directory'].int(), self['offset of start of central directory with respect to the starting disk number']) + ("// %s"%self['.ZIP file comment'].str() if self['.ZIP file comment length'].int() > 0 else '')
 
-@ZipRecord.define(signature=(64,0x06054b50))
+@ZipRecord.define(signature=(64, 0x06054b50))
 class EndOfCentralDirectory64(pstruct.type):
     def __ExtensibleDataSector(self):
         size = EndOfCentralDirectory().a.size()
@@ -229,7 +229,7 @@ class EndOfCentralDirectory64(pstruct.type):
         data = self['zip64 extensible data sector'].summary()
         return '{:s}) {:s} version-made-by={:s} version-needed-to-extract={:s} number-of-the-disk-with-the-start-of-the-zip64-end-of-central-directory={:d} number-of-the-disk-with-the-start-of-the-central-directory={:d} total-number-of-entires-in-the-central-directory-on-this-disk={:d} total-number-of-entries-in-the-central-directory={:d} size-of-the-central-directory=0x{:x} offset-of-start-of-central-directory-with-respect-to-the-starting-disk-number={:d}'.format(index, cls, self['number of the disk with the start of the zip64 end of central directory'].int(), self['number of the disk with the start of the central directory'].int(), self['total number of entries in the central directory on this disk'].int(), self['total number of entries in the central directory'].int(), self['size of the central directory'].int(), self['offset of start of central directory with respect to the starting disk number'].int(), datasector)
 
-@ZipRecord.define(signature=(64,0x07054b50))
+@ZipRecord.define(signature=(64, 0x07054b50))
 class EndOfCentralDirectoryLocator64(pstruct.type):
     _fields_ = [
         (pint.uint32_t, 'number of the disk with the start of the zip64 end of central directory'),
@@ -245,7 +245,7 @@ class EndOfCentralDirectoryLocator64(pstruct.type):
         cls = self.classname()
         return '{:s}) {:s} number-of-the-disk-with-the-start-of-the-zip64-end-of-central-directory={:d} relative-offset-of-the-zip64-end-of-central-directory-record=0x{:x} total-number-of-disks={:d}'.format(index, cls, self['number of the disk with the start of the zip64 end of central directory'].int(), self['relative offset of the zip64 end of central directory record'].int(), self['total number of disks'].int())
 
-@ZipRecord.define(signature=(32,0x08064b50))
+@ZipRecord.define(signature=(32, 0x08064b50))
 class ArchiveExtraData(pstruct.type):
     _fields_ = [
         (pint.uint32_t, 'extra field length'),
@@ -260,7 +260,7 @@ class ArchiveExtraData(pstruct.type):
         cls = self.classname()
         return '{:s}) extra-field-length={:d} extra-field={:s}'.format(index, cls, self['extra field length'].int(), self['extra field data'].summary())
 
-@ZipRecord.define(signature=(32,0x05054b50))
+@ZipRecord.define(signature=(32, 0x05054b50))
 class DigitalSignature(pstruct.type):
     _fields_ = [
         (pint.uint16_t, 'size of data'),
@@ -294,23 +294,25 @@ class File(parray.infinite):
         return value.__class__ == EndOfCentralDirectory
 
 if __name__ == '__main__':
-    import sys,os,os.path,zlib,logging,argparse
-    import ptypes,archive.zip
+    import sys, os, os.path
+    import zlib, logging, argparse
+    import ptypes, archive.zip
     if sys.platform == 'win32': import msvcrt
 
     arg_p = argparse.ArgumentParser(prog=sys.argv[0] if len(sys.argv) > 0 else 'zip.py', description="List or extract information out of a .zip file", add_help=False)
     arg_p.add_argument('FILE', nargs='*', action='append', type=str, help='list of filenames to extract')
-    arg_commands_gr = arg_p.add_argument_group("Main operation mode")
+    arg_p.add_argument('-v', '--verbose', action='store_true', help="output verbose logging information", dest='verbose')
+    arg_commands_gr = arg_p.add_argument_group("main operation mode")
     arg_commands_gr.add_argument('-h', '--help', action='store_const', help="show this help message and exit", dest='mode', const='help')
     arg_commands_gr.add_argument('-l', '--list', action='store_const', help="list the contents of an archive", dest='mode', const='list')
-    arg_commands_gr.add_argument('-x', '--extract', '--get', action='store_const', help="extract files from an archive", dest='mode', const='extract')
+    arg_commands_gr.add_argument('-la', '--list-all', action='store_const', help="list the entire contents of an archive", dest='mode', const='list-all')
+    arg_commands_gr.add_argument('-x', '--extract', '--get', action='store_const', help="extract the specified file records", dest='mode', const='extract')
     arg_commands_gr.add_argument('-d', '--dump', action='store_const', help="dump the specified file records", dest='mode', const='dump')
-    arg_device_gr = arg_p.add_argument_group("Device selection and switching")
+    arg_device_gr = arg_p.add_argument_group("device selection and switching")
     arg_device_gr.add_argument('-f', '--file', nargs=1, action='store', type=str, metavar="ARCHIVE", help="use archive file or device ARCHIVE", dest='source')
     arg_device_gr.add_argument('-o', '--output', nargs=1, action='store', type=str, metavar="DEVICE", help="extract files to specified DEVICE or FORMAT", dest='target', default=None)
-    arg_info_gr = arg_p.add_argument_group("Format output")
-    arg_info_gr.add_argument('-v', '--verbose', action='store_true', help="verbosely list entire contents of archive", dest='verbose')
-    arg_info_gr.add_argument('-j', '--compressed', action='store_true', help="extract data from archive in compressed form", dest='compress', default=False)
+    arg_info_gr = arg_p.add_argument_group("format output")
+    arg_info_gr.add_argument('-j', '--compressed', action='store_true', help="extract data from archive in its compressed form", dest='compress', default=False)
 
     if len(sys.argv) <= 1:
         print >>sys.stdout, arg_p.format_usage()
@@ -322,7 +324,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     # fix up arguments
-    source_a,target_a = args.source[0],None if args.target is None else args.target[0]
+    source_a, target_a = args.source[0], None if args.target is None else args.target[0]
     if source_a == '-':
         if sys.platform == 'win32': msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
         source = ptypes.prov.stream(sys.stdin)
@@ -339,15 +341,14 @@ if __name__ == '__main__':
     filelookup = set(filelist)
     indexlookup = set((int(n) for n in filelist if n.isdigit()))
 
-    verbose = args.verbose
-    isMatchedName = lambda r: (not filelookup) or ('Filename' in r['Record'].keys() and r['Record']['Filename'].str() in filelookup)
+    isMatchedName = lambda r: (not filelookup) or ('file name' in r['Record'].keys() and r['Record']['file name'].str() in filelookup)
     isMatchedIndex = lambda r: int(r.name()) in indexlookup
     isMatched = lambda r: isMatchedName(r) or isMatchedIndex(r)
 
     # some useful functions
     def iterate(root):
         for rec in root:
-            if verbose and isMatched(rec):
+            if args.mode == 'list-all' and isMatched(rec):
                 yield rec
             elif isMatchedIndex(rec):
                 yield rec
@@ -372,17 +373,26 @@ if __name__ == '__main__':
             continue
         return res
 
-    # create the file type
+    # set the verbosity
+    if args.verbose:
+        logging.root.setLevel(logging.INFO)
+
+    # create the file instance
     z = archive.zip.File(source=source)
 
-    # implement each mode
+    # handle the mode that the user specified
     if args.mode == 'list':
         for rec in iterate(z.l[:-1]):
             print rec['Record'].listing()
         sys.exit(0)
 
+    elif args.mode == 'list-all':
+        for rec in iterate(z.l[:-1]):
+            print rec['Record'].listing()
+        sys.exit(0)
+
     elif args.mode == 'extract':
-        target = target or os.path.join('.','{path:s}','{name:s}')
+        target = target or os.path.join('.', '{path:s}', '{name:s}')
 
     elif args.mode == 'dump':
         target = target or sys.stdout
@@ -394,39 +404,41 @@ if __name__ == '__main__':
 
     # for each record...
     for rec in iterate(z.l[:-1]):
-
         # assign what data we're writing
         if args.mode == 'extract':
             data = rec['Record'].extract(decompress=not args.compress)
         elif args.mode == 'dump':
-            data = '\n'.join((' '.join((ptypes.utils.repr_class(rec.classname()),rec.name())), ptypes.utils.indent(repr(rec['Record']))))
+            data = '\n'.join((' '.join((ptypes.utils.repr_class(rec.classname()), rec.name())), ptypes.utils.indent("{!r}".format(rec['Record']))))
+        else:
+            raise NotImplementedError(args.mode)
 
-        # set some reasonable defaults
+        # set some reasonable defaults for formatting
         res = dictionary(rec['Record'])
         res['index'] = int(rec.name())
-        res['path'],res['name'] = os.path.split(res['file name'] if 'file name' in res.keys() else rec.name())
+        res['path'], res['name'] = os.path.split(res['file name'] if 'file name' in res.keys() else rec.name())
 
         # write to a generated filename
         if isinstance(target, basestring):
             outname = target.format(**res)
 
-            dirpath,name = os.path.split(outname)
+            dirpath, name = os.path.split(outname)
             dirpath and not os.path.isdir(dirpath) and os.makedirs(dirpath)
 
             res = os.path.join(dirpath, name)
             if res.endswith(os.path.sep):
                 if os.path.isdir(res):
-                    logging.warn("Unable to create already existing subdirectory for record : {:d} : {:s}".format(int(rec.name()), res))
+                    logging.warn("Refusing to overwrite already existing subdirectory for record({:d}) : {:s}".format(int(rec.name()), res))
                 else:
-                    logging.info("Creating subdirectory due to record : {:d} : {:s}".format(int(rec.name()), res))
+                    logging.info("Creating subdirectory for record({:d}) : {:s}".format(int(rec.name()), res))
                     os.makedirs(res)
                 continue
 
             if os.path.exists(res):
-                logging.warn("Overwriting already existing file due to record : {:d} : {:s}".format(int(rec.name()), res))
+                logging.warn("Overwriting already existing file with record({:d}) : {:s}".format(int(rec.name()), res))
             else:
-                logging.info("Creating file for record : {:d} : {:s}".format(int(rec.name()), res))
+                logging.info("Creating new file for record({:d}) : {:s}".format(int(rec.name()), res))
 
+            logging.info("{:s}ing {:d} bytes from record({:d}) to file: {:s}".format(args.mode.title(), len(data), int(rec.name()), res))
             with file(res, 'wb') as out: print >>out, data
 
         # fall-back to writing to already open target
