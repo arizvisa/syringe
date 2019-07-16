8f777d70e93cb9ecb68a292b88daae6e9a95ed64
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-07-16 00:01:48 -0500

    Reorganized the entries in the pecoff.portable.relocations library so that they're using a ptype.definition for determining the relocation type.

--- lib/pecoff/portable/relocations.py
+++ lib/pecoff/portable/relocations.py
@@ -7,10 +7,15 @@ class MachineRelocation(ptype.definition):
 
 ## Base Relocation class (Sub-class for each machine type)
 class Relocation(pstruct.type):
+    def __Type(self):
+        header = LocateHeader(self)
+        res = header['FileHeader']
+        return MachineRelocation.lookup(res['Machine'].int())
+
     _fields_ = [
         (uint32, 'VirtualAddress'), # FIXME: this can be a virtualaddress(...) to the actual relocation
         (uint32, 'SymbolTableIndex'),
-        #(uint16, 'Type')
+        (__Type, 'Type')
     ]
 
     def summary(self):
@@ -18,15 +23,14 @@ class Relocation(pstruct.type):
         res = ', '.join(['%s=%s'% (k,t(self[k])) for k,t in fields])
         return '{' + res + '}'
 
-    def __relocate__(self, data, symbol, section, namespace):
-        raise NotImplementedError
-
     def relocate(self, data, symboltable, namespace=None):
         '''
         data = a string
         symboltable = an array of symbols
         namespace = a dict for looking up symbol and segment values by name
         '''
+        raise NotImplementedError('This has been deprecated due to a refactor')
+
         # FIXME: stupid fucking python and it's issues recursive module importing
         headers = sys.modules.get('pecoff.portable.headers', __import__('pecoff.portable.headers'))
 
@@ -47,12 +51,15 @@ class Relocation(pstruct.type):
 
         # convert the data into an array that can be processed
         data = array.array('B', data)
-        return self.__relocate__(data, symbol, section, namespace).tostring()
+        return self['Type'].__relocate__(data, symbol, section, namespace).tostring()
 
 ### Each relocation entry
 
 ## I386
+@MachineRelocation.define
 class IMAGE_REL_I386(pint.enum, uint16):
+    type = Machine.byname('I386')
+
     _values_ = [
         ('ABSOLUTE', 0x0000),
         ('DIR16', 0x0001),
@@ -67,14 +74,10 @@ class IMAGE_REL_I386(pint.enum, uint16):
         ('REL32', 0x0014)
     ]
 
-@MachineRelocation.define
-class Relocation_I386(Relocation):
-    type = Machine.byname('I386')
-
-    _fields_ = Relocation._fields_[:] + [(IMAGE_REL_I386, 'Type')]
-
     def __relocate__(self, data, symbol, section, namespace=None):
         '''Apply relocations for the specified ``symbol`` to the ``data``.'''
+        raise NotImplementedError('This internal method has been deprecated due to a refactor')
+
         namespace = namespace or {}
 
         # figure out the relocation information
@@ -135,7 +138,9 @@ class Relocation_I386(Relocation):
         return data
 
 ## AMD64
+@MachineRelocation.define
 class IMAGE_REL_AMD64(pint.enum, uint16):
+    type = Machine.byname('AMD64')
     _values_ = [
         ('ABSOLUTE', 0x0000),
         ('ADDR64', 0x0001),
@@ -156,33 +161,117 @@ class IMAGE_REL_AMD64(pint.enum, uint16):
         ('SSPAN32', 0x0010),
     ]
 
-@MachineRelocation.define
-class Relocation_AMD64(Relocation):
-    type = Machine.byname('AMD64')
-
-    _fields_ = Relocation._fields_[:] + [(IMAGE_REL_AMD64, 'Type')]
-
     def __relocate__(self, data, symbol, section, namespace=None):
         '''Apply relocations for the specified ``symbol`` to the ``data``.'''
-        # FIXME: Implement this
+        raise NotImplementedError('This internal method has been deprecated due to a refactor')
         return data
 
 ## per data directory relocations
+class RelocationType(ptype.definition):
+    cache = {}
+
+class RelocationTypeBase(pbinary.integer):
+    def blockbits(self):
+        return 12
+
+    def read(self, data, offset, length):
+        '''Given the specified data, read length bytes from the provided offset and return as a little-endian integer.'''
+        res = array.array('B', data[offset : offset + length])
+        return reduce(lambda agg, item: agg * 0x100 + item, reversed(res), 0)
+
+    def write(self, integer, length):
+        '''Given the specified integer and its length, encode it into its little-endian form and return a string.'''
+        res = array.array('B')
+        binteger = bitmap.new(integer, 8 * length)
+        octets = bitmap.split(binteger, 8)
+        for item in reversed(octets):
+            res.append(bitmap.int(item))
+        return res.tostring()
+
+@RelocationType.define(type=1)
+class RelocationType1(RelocationTypeBase):
+    def read(self, data, offset):
+        return super(RelocationType1, self).read(data, offset, 2)
+
+    def write(self, address, delta):
+        res = address + (delta & 0xffff0000) / 0x10000
+        return super(RelocationType1, self).write(res, 2)
+
+@RelocationType.define(type=2)
+class RelocationType2(RelocationTypeBase):
+    def read(self, data, offset):
+        return super(RelocationType2, self).read(data, offset, 2)
+
+    def write(self, address, delta):
+        res = address + (delta & 0x0000ffff)
+        return super(RelocationType2, self).write(res, 2)
+
+@RelocationType.define(type=3)
+class RelocationType3(RelocationTypeBase):
+    def read(self, data, offset):
+        return super(RelocationType3, self).read(data, offset, 4)
+
+    def write(self, address, delta):
+        res = (address + delta) & 0xffffffff
+        return super(RelocationType3, self).write(res, 4)
+
+@RelocationType.define(type=10)
+class RelocationType10(RelocationTypeBase):
+    def read(self, data, offset):
+        return super(RelocationType10, self).read(data, offset, 8)
+
+    def write(self, address, delta):
+        res = address + (delta & 0xffffffffffffffff)
+        return super(RelocationType10, self).write(res, 8)
+
 class BaseRelocationEntry(pbinary.struct):
+    def __Offset(self):
+        res = self['Type']
+        return RelocationType.lookup(res)
+
     _fields_ = [
         (4, 'Type'),
-        (12, 'Offset'),
+        (__Offset, 'Offset'),
     ]
+
+    def apply(self, entry, imagebase, sectiontable, segment, **addresses):
+        '''
+        Apply the current relocation to the provided segment (data) using the
+        specified IMAGE_BASERELOC_DIRECTORY_ENTRY along with the imagebase to
+        convert the relocation target address into an offset, and the provided
+        section table to calculate the new relocated address that is written.
+        If any section names are provided as extra arguments, then use that as
+        the segment base address instead.
+        '''
+        relocation, page = self.item('Offset'), entry['Page RVA'].int()
+        section = sectiontable.getsectionbyaddress(page)
+
+        # FIXME: I haven't actually verified this is working correctly since
+        #        it's not actually being used yet since the refactor.
+
+        res = relocation.read(segment, page - section['VirtualAddress'].int() + relocation.int())
+        target = res - imagebase
+        section = sectiontable.getsectionbyaddress(target)
+
+        offset = target - section['VirtualAddress'].int()
+        res = relocation.write(offset, addresses.get(section['Name'].str(), section['VirtualAddress'].int()))
+
+        segment[offset : offset + len(res)] = array.array('B', res)
+
+        return segment
 
 class BaseRelocationArray(parray.block):
     _object_ = pbinary.littleendian(BaseRelocationEntry)
 
 class BaseRelocationBlock(ptype.block):
     def array(self):
+        '''Return this type casted to a BaseRelocationArray with the correct types.'''
         return self.cast(pbinary.new(BaseRelocationArray), blocksize=self.blocksize)
     def iterate(self):
-        for res in iter(self.array()):
-            yield res
+        '''Iterate through each relocation wrapped by this type.'''
+        res = self.array()
+        for item in iter(res):
+            yield item
         return
 
 class IMAGE_BASERELOC_DIRECTORY_ENTRY(pstruct.type):
@@ -211,50 +300,6 @@ class IMAGE_BASERELOC_DIRECTORY_ENTRY(pstruct.type):
             yield type, pageoffset + offset
         return
 
-# yeah i really made this an object
-class relocationtype(object):
-    length = 0
-    def read(self, string, offset):
-        value = reversed( string[offset : offset+self.length] )
-        result = reduce(lambda total,x: total*256 + ord(x), value, 0)
-        return result
-
-    def write(self, address, sectiondelta):
-        raise NotImplementedError
-
-    def _write(self, value):
-        res = bitmap.new( value, self.length*8 )
-        string = ''
-        while res[1] > 0:
-            res,value = bitmap.consume(res, 8)
-            string += chr(value)
-        return string
-
-if False:       # deprecated because we would like to be able to separate segments from one another
-    class relocation_1(relocationtype):
-        length = 2
-        def write(self, address, sectiondelta):
-            value = address + (sectiondelta & 0xffff0000) / 0x10000
-            return super(relocation_1, self)._write(value)
-
-    class relocation_2(relocationtype):
-        length = 2
-        def write(self, address, sectiondelta):
-            value = address + (sectiondelta & 0x0000ffff)
-            return super(relocation_2, self)._write(value)
-
-    class relocation_3(relocationtype):
-        length = 4
-        def write(self, address, sectiondelta):
-            value = (address + sectiondelta) & 0xffffffff
-            return super(relocation_3, self)._write(value)
-
-    class relocation_10(relocationtype):
-        length = 8
-        def write(self, address, sectiondelta):
-            value = address + (sectiondelta & 0xffffffffffffffff)
-            return super(relocation_10, self)._write(value)
-
 class IMAGE_BASERELOC_DIRECTORY(parray.block):
     _object_ = IMAGE_BASERELOC_DIRECTORY_ENTRY
     def filter(self, section):
@@ -271,23 +316,23 @@ class IMAGE_BASERELOC_DIRECTORY(parray.block):
             raise AssertionError("Type of argument `data` must of an instance of {!r} : not isinstance({!r}, array.array)".format(array.array, data.__class__))
 
         sectionname = section['Name'].str()
-        imagebase = self.getparent(Header)['OptionalHeader']['ImageBase'].int()
+        header = LocateHeader(self)
+        imagebase = header['OptionalHeader']['ImageBase'].int()
 
         sectionarray = section.parent
         sectionvaLookup = dict( ((s['Name'].str(),s['VirtualAddress'].int()) for s in sectionarray) )
 
         # relocation type 3
-        t = relocationtype()
-        t.length = 4
-        t.write = lambda offset, base: t._write(base + offset)
+        res = RelocationType.lookup(3)
+        R = res()
 
         for entry in self.filter(section):
-            for type,offset in entry.getrelocations(section):
+            for type, offset in entry.getrelocations(section):
                 if type != 3:
                     raise NotImplementedError("Relocations other than type 3 aren't implemented because I couldn't find any to test with")
                 currentva = sectionvaLookup[sectionname]+offset
 
-                targetrva = t.read(data, offset)
+                targetrva = R.read(data, offset)
                 targetva = targetrva-imagebase
 
                 try:
@@ -304,9 +349,9 @@ class IMAGE_BASERELOC_DIRECTORY(parray.block):
                 targetsectionname = targetsection['Name'].str()
                 targetoffset = targetva - sectionvaLookup[targetsectionname]
 
-#                relocation = t.write(targetva, imagebase)
+#                relocation = R.write(targetva, imagebase)
                 # apply the relocation to the data that was passed to us
-                relocation = t.write(targetoffset, namespace[targetsectionname])
+                relocation = R.write(targetoffset, namespace[targetsectionname])
                 data[offset : offset + len(relocation)] = array.array('c', relocation)
             continue
 
