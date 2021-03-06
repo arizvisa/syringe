b4e9d7cbe2295101e9e6277bd0a593eeb0dd446e
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-03-05 19:03:54 -0600

    Removed a duplicate import from the `pecoff.portable` library module.

--- lib/pecoff/portable/__init__.py
+++ lib/pecoff/portable/__init__.py
@@ -1,3 +1,3 @@
-from . import headers,datadirectory,exceptions,exports,headers,imports,linenumbers,relocations,resources,symbols,clr
+from . import headers,datadirectory,exceptions,exports,imports,linenumbers,relocations,resources,symbols,clr
 from .datadirectory import DataDirectory
 from .headers import IMAGE_FILE_HEADER,IMAGE_OPTIONAL_HEADER,IMAGE_OPTIONAL_HEADER64,SectionTableArray,SectionTable
