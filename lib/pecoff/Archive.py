f1c437e9032a6fee31a852ad0f25ca8629c24223
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-10-02 07:07:18 -0500

    Modified the pecoff.Archive library module to remove the usage of ptype.definition for looking up member types.

--- lib/pecoff/Archive.py
+++ lib/pecoff/Archive.py
@@ -1,4 +1,6 @@
-import logging, itertools, ptypes
+import logging, itertools, datetime
+
+import ptypes
 from ptypes import *
 
 from . import Object
@@ -22,28 +24,43 @@ class stringinteger(pstr.string):
         except Exception as E:
             logging.warning("Returning {:d} due to the inability to convert the string ({!r}) at {:s} to an integer.".format(0, string, self.instance()))
         return result
-
     int = get
 
 class stringdate(stringinteger):
-    # FIXME: implement some of the methods that conver this instance
-    #        into a proper date that we can interact with.
     def datetime(self):
-        string = self.get()
-        raise NotImplementedError
+        seconds = self.get()
+        epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc if hasattr(datetime, 'timezone') else None)
+        delta = datetime.timedelta(seconds=seconds)
+        raise epoch + delta
 
 class Index(pint.uint16_t):
     def GetIndex(self):
         return self.int() - 1      # 1 off
 
+class IMPORT_TYPE(pbinary.enum):
+    length, _values_ = 2, [
+        ('CODE', 0),
+        ('DATA', 1),
+        ('CONST', 2),
+    ]
+
+class IMPORT_NAME_TYPE(pbinary.enum):
+    length, _values_ = 3, [
+        ('ORDINAL', 0),
+        ('NAME', 1),
+        ('NAME_NOPREFIX', 2),
+        ('NAME_UNDECORATE', 3),
+    ]
+
 class Import(pstruct.type):
     class Header(pstruct.type):
         class Type(pbinary.struct):
             _fields_ = [
-                (2, 'Type'),
-                (3, 'Name Type'),
+                (IMPORT_TYPE, 'Type'),
+                (IMPORT_NAME_TYPE, 'Name Type'),
                 (11, 'Reserved')
             ]
+
         _fields_ = [
             (pint.uint16_t, 'Sig1'),
             (pint.uint16_t, 'Sig2'),
@@ -55,15 +72,15 @@ class Import(pstruct.type):
             (Type, 'Type')
         ]
 
+        def valid(self):
+            sig1,sig2 = self['Sig1'].int(), self['Sig2'].int()
+            return sig1 == 0 and sig2 == 0xffff
+
     class Member(pstruct.type):
         _fields_ = [
             (pstr.szstring, 'Name'),
             (pstr.szstring, 'Module')
         ]
-
-        def valid(self):
-            sig1,sig2 = self['Sig1'].int(), self['Sig2'].int()
-            return sig1 == 0 and sig2 == 0xffff
 
         def repr(self):
             if self.initializedQ():
@@ -78,26 +95,7 @@ class Import(pstruct.type):
     def GetImport(self):
         return self['Member']['Module'].str(), self['Member']['Name'].str(), self['Header']['Ordinal/Hint'].int(), self['Header']['Type']
 
-#######
-class MemberType(ptype.definition):
-    attribute, cache = 'internalname', {}
-
-    class Data(dynamic.union):
-        _fields_ = [
-            (Import, 'Import'),
-            (Object.File, 'Object'),
-            (ptype.block, 'Block'),
-        ]
-
-        def isImport(self):
-            return self['Import']['Header'].valid()
-
-    default = Data
-
-## Linker objects
-@MemberType.define
 class Linker1(pstruct.type):
-    internalname = 0
     _fields_ = [
         (ulong, 'Number of Symbols'),
         (lambda self: dyn.array(ulong, self['Number of Symbols'].li.int()), 'Offsets'),
@@ -111,9 +109,7 @@ class Linker1(pstruct.type):
             table.append(item)
         return table
 
-@MemberType.define
 class Linker2(pstruct.type):
-    internalname = 1
     _fields_ = [
         (pint.uint32_t, 'Number of Members'),
         (lambda self: dyn.array(pint.uint32_t, self['Number of Members'].li.int()), 'Offsets'),
@@ -131,27 +127,19 @@ class Linker2(pstruct.type):
             table.append(item)
         return table
 
-#@MemberType.define
-class LinkerMember1(Linker1):
-    internalname = "/"
-@MemberType.define
-class LinkerMember2(Linker2):
-    internalname = "/"
-
-@MemberType.define
 class Longnames(ptype.block):
-    internalname = "//"
-
     def extract(self, index):
         data = self.serialize()[index:]
         bytes = ptypes.utils.strdup(data, terminator=b'\0')
-        return bytes
-        #return bytes.decode('latin1')      # FIXME: probably a better idea to decode this using the correct encoding
+        return bytes.decode('latin1')
 
-#######
+class Data(ptype.encoded_t):
+    def _object_(self):
+        header_t = Import.Header
+        header = self.new(header_t, source=ptypes.provider.proxy(self)).l
+        return Import if header.valid() else Object.File
+
 class Member(pstruct.type):
-    internalname = None
-
     class Header(pstruct.type):
         class Name(pstr.string):
             length = 16
@@ -159,6 +147,10 @@ class Member(pstruct.type):
                 string = super(Member.Header.Name, self).get()
                 return string.rstrip()
             str = get
+
+            # FIXME: this string has the format "/%d" where the integer
+            #        that it contains represents the offset into the
+            #        Longnames member.
 
         _fields_ = [
             (Name, 'Name'),
@@ -174,22 +166,24 @@ class Member(pstruct.type):
             size = self['Size'].int()
             return self.new(dyn.block(self['Size'].int()), __name__=self['Name'].str(), offset=self.getoffset() + self.size())
 
-    __LinkerMember__ = None
-    def __Data(self):
-        header = self['Header'].li
-
-        # If the __LinkerMember__ property is defined as something, then use it for our type
-        if self.__LinkerMember__:
-            res, size = self.__LinkerMember__, header['Size'].int()
-
-        # Otherwise, we use the member name in order to determine the type.
+    def _Member_(self, name, size):
+        '''This is the default callable that returns the type based on the name.'''
+        if name == '/':
+            result = Linker2
+        elif name == '//':
+            result = Longnames
         else:
-            filename, size = header['Name'].str(), header['Size'].int()
-            default = dyn.clone(MemberType.default, internalname=filename, _value_=dyn.block(size))
-            res = MemberType.lookup(filename, default)
+            result = dyn.clone(Data, _value_=dyn.block(size))
+        return result
 
-        # If our type is related to a block, then clone it using the size from the header.
-        return dyn.clone(res, length=size) if ptype.isrelated(res, ptype.block) else res
+    def __Member(self):
+        header = self['Header'].li
+        name, size = (header[fld] for fld in ['Name', 'Size'])
+
+        # Use the header attributes that we fetched in order to pass
+        # to the closure and determine the member type.
+        callable = self._Member_
+        return callable if ptype.istype(callable) else callable(name.str(), size.int())
 
     def __newline(self):
         ofs = self.getoffset('Member') + self['Header'].li['Size'].int()
@@ -200,7 +194,7 @@ class Member(pstruct.type):
 
     _fields_ = [
         (Header, 'Header'),
-        (__Data, 'Member'),
+        (__Member, 'Member'),
         (__newline, 'newline'),
     ]
 
@@ -208,47 +202,86 @@ class Members(parray.terminated):
     # FIXME: it'd be supremely useful to cache all the import and object records
     #        somewhere instead of assigning them directly to Linker and Names
 
-    Linker, Names = None, None
+    def __init__(self, **attrs):
+        self.Linker = self.Names = None
+        return super(Members, self).__init__(**attrs)
 
     def _object_(self):
+        def closure(name, size, index=len(self.value)):
 
-        # Hardcode the first two members of our archive
-        if len(self.value) < 2:
-            index = len(self.value)
-            t = MemberType.lookup(index)
-            return dyn.clone(Member, __LinkerMember__=t)
+            # Hardcode the first member of our archive
+            if index == 0:
+                if name != '/':
+                    raise NameError(index, name)
+                result = Linker1
 
-        # Otherwise we can determine the type by using the name
-        return Member
+            # Otherwise we can determine the type by using the name
+            elif name == '/':
+                result = Linker2
+            elif name == '//':
+                result = Longnames
+            else:
+                result = dyn.clone(Data, _value_=dyn.block(size))
+            return result
+        return dyn.clone(Member, _Member_=staticmethod(closure))
 
     def isTerminator(self, value):
-        name = value['Header']['Name'].str()
+        count, name = len(self.value), value['Header']['Name']
 
-        # Always read at least 2 members. If one of them has a member named "/"
-        # then we cache it in the "Linker" property.
-        if len(self.value) <= 2:
-            if name == '/':
-                self.Linker = value['Member']
+        # Always read at least one member.
+        if not count:
             return False
 
-        # If we find a member named "//" then cache it in the "Names" property.
-        if name == '//':
-            self.Names = value
+        # Otherwise if our member name is '/', then we need to cache it.
+        elif name.str() == '/' and isinstance(value['Member'], Linker2):
+            self.Linker = value['Member']
 
-        res = self.Linker['Number of Members'].int()
-        return False if len(self.value) < res else True
+        # If the name is '//', then we need to cache that too so that we
+        # can figure out the member's actual (long) name.
+        elif name.str() == '//' and isinstance(value['Member'], Longnames):
+            self.Names = value['Member']
+
+        # If our source is bounded, then we calculate the bounds so that
+        # we can continue to read all of the available members even if
+        # the file is malformed.
+        if isinstance(self.source, ptypes.provider.bounded):
+
+            # If our Linker2 member was assigned, then check whether we've
+            # read a number of members matching the 'Number of Members' field.
+            if self.Linker:
+                return count >= 2 + self.Linker['Number of Members'].int()
+
+            # Otherwise, continue to read members while there's still data
+            # that we can read from the source. This should guarantee reading
+            # the Linker2 member since it should be near the beginning.
+            bounds = value.getoffset() + value.size()
+            return bounds >= self.source.size()
+
+        # If our source is unbounded, then we need to figure out whether
+        # we've read the Linker2 member yet so that we can check the number
+        # of members that we've read against it 'Number of Members' field.
+        if self.Linker:
+            return count >= 2 + self.Linker['Number of Members'].int()
+
+        # Otherwise we always read at least 2 members because the first two
+        # should always be Linker1 followed by Linker2.
+        return count >= 2
 
     def iterate(self):
-        for m in (x['Member'] for x in self if isinstance(x, ArchiveMember)):
-            if m.isImport():
+        iterable = (item['Member'] for item in self if isinstance(item['Member'], Data))
+        for item in iterable:
+            member = item.d.li
+            if isinstance(member, Import):
                 continue
-            yield m['Object']
+            yield member
         return
 
     def iterate_imports(self):
-        for m in (x['Member'] for x in self if isinstance(x, ArchiveMember)):
-            if m.isImport():
-                yield m['Import']
+        iterable = (item['Member'] for item in self if isinstance(item['Member'], Data))
+        for item in iterable:
+            member = item.d.li
+            if isinstance(member, Import):
+                yield member
             continue
         return
 
@@ -261,14 +294,22 @@ class File(pstruct.type):
 
     ## really slow interface
     def getmember(self, index):
-        offsets = self['members'].Linker['Offsets']
-        return self.new(Member, __name__="member[{:d}]".format(index), offset=offsets[index])
+        members = self['members']
+        if not members.Linker:
+            raise ptypes.error.InitializationError(self, 'getmember')
+        offsets = members.Linker['Offsets']
+        if index == 0:
+            return self.new(Member, __name__="member[{:d}]".format(index), offset=offsets[index].int(), _Member_=Linker1)
+        return self.new(Member, __name__="member[{:d}]".format(index), offset=offsets[index].int())
 
     def getmemberdata(self, index):
         return self.getmember(index).li.data().l.serialize()
 
     def getmembercount(self):
-        return self['members'].Linker['Number of Members'].int()
+        members = self['members']
+        if members.Linker:
+            return members.Linker['Number of Members'].int()
+        raise ptypes.error.InitializationError(members, 'getmembercount')
 
     ## faster interface using ptypes to slide view
     def fetchimports(self):
@@ -313,31 +354,21 @@ class File(pstruct.type):
         return
 
 if __name__ == '__main__':
-    import pecoff.Archive as Archive
+    import sys
+    import ptypes, pecoff.Archive as Archive
     from ptypes import *
-    source = ptypes.file('~/python26/libs/python26.lib')
+    source = ptypes.setsource(ptypes.prov.file(sys.argv[1]))
 
-    print('Reading .lib header')
-#    Archive.File = ptypes.debugrecurse(Archive.File)
+    print("Reading contents of {:s}...".format(source.file.name))
     self = Archive.File()
-#    self.source = provider.file('../../obj/test.lib')
-    self.source = ptypes.file('~/python26/libs/python26.lib')
     self.load()
+    print("Successfuly read {:d} members.".format(len(self['members'])))
 
-#    print(self['SymbolNames']['Header'])
-#    print(self['SymbolNames']['Member'])
-#    print(self['MemberNames']['Header'])
-#    print(self['MemberNames']['Member'])
-#    print(self['LongNames']['Header'])
-#    print(self['LongNames']['Member'])
-#    print('-'*79)
+    # TODO: enumerate all objects that are dll imports
+    # TODO: enumerate all objects that are actual object files
 
-    ## enumerate all objects that are dll imports
-    ## enumerate all objects that are actual object files
-
-    print('enumerating all members')
-    for index in xrange( self.getmembercount() ):
-        print(self.getmember(index).load())
-        print(ptypes.utils.hexdump(self.getmemberdata(index)))
-
-#    a = Archive.File(offset=19912)
+    #print('Enumerating all members using view interface...')
+    #for index in range(self.getmembercount()):
+    #    print(self.getmember(index).load())
+    #    print(ptypes.utils.hexdump(self.getmemberdata(index)))
+    sys.exit(0)
