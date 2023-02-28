arizvisa/syringe

commit 750e59514818ee265101f1f820fe7979aec28622

Hacked up the summaries from the ptypes library to avoid serializing data that isn't needed for display.

lib/ptypes/ptype.py
@@ -227,7 +227,7 @@ Example pointer_t usage:
         def _calculate_(self, number):
             return number + 0x100
 """
-import sys, builtins, functools, itertools, types
+import sys, builtins, functools, itertools, types, operator
 import time
 
 from . import bitmap, provider, utils, error
@@ -613,20 +613,33 @@ class __interface__(object):
         if self.value is None:
             return u"???"
 
-        data = self.serialize()
+        threshold = options.pop('threshold', Config.display.threshold.summary)
+        message = options.pop('threshold_message', Config.display.threshold.summary_message)
+        #threshold = options.pop('width', threshold) # 'threshold' maps to 'width' for emit_repr
+
         try: size = self.size()
         except Exception: size = self.blocksize()
+
+        if threshold < 0 or size < threshold:
+            data = self.serialize()
+            return u'"{:s}"'.format(utils.emit_repr(data[:size], **options))
+
+        if builtins.isinstance(self.value, bytes) or size < threshold:
+            data = self.serialize() if self.initializedQ() else self.value
+
+        # if there's too much data, the slice it together from both sides
+        else:
+            lefti, righti = utils.valueaccumulate(self.value, +1), utils.valueaccumulate(self.value, -1)
+            leftitems = [next(lefti)[1]] + [item for _, item in itertools.takewhile(lambda offset_data: offset_data[0] < threshold, lefti)]
+            rightitems = [next(righti)[1]] + [item for _, item in itertools.takewhile(lambda offset_data: offset_data[0] < threshold, righti)]
+            data = bytes().join([bytes().join(leftitems), bytes().join([item for item in rightitems][::-1])])
+            options['size'] = size
 
         options.setdefault('offset', self.getoffset())
 
         # if larger than threshold...
-        threshold = options.pop('threshold', Config.display.threshold.summary)
-        message = options.pop('threshold_message', Config.display.threshold.summary_message)
-        if threshold > 0 and size > threshold:
-            threshold = options.pop('width', threshold) # 'threshold' maps to 'width' for emit_repr
-            res = utils.emit_repr(data[:size], threshold, message, **options)
-        else:
-            res = utils.emit_repr(data[:size], **options)
+        assert(threshold > 0 and size > threshold)
+        res = utils.emit_repr(data, threshold, message, **options)
         return u'"{:s}"'.format(res)
 
     #@utils.memoize('self', self='parent', args=lambda item: (item[0],) if len(item) > 0 else (), kwds=lambda item: item.get('type', ()))
@@ -1534,13 +1547,11 @@ class container(base):
         threshold = options.pop('threshold', Config.display.threshold.summary)
         message = options.pop('threshold_message', Config.display.threshold.summary_message)
         if self.value is not None:
-            def blocksizeorelse(self):
-                try: res = self.blocksize()
-                except Exception: res = 0
-                return res
-
-            #data = ''.join([item.serialize() if item.initializedQ() else '?'*blocksizeorelse(item)] for item in self.value)
-            data = b''.join(item.serialize() if item.initializedQ() else b'' for item in self.value)
+            lefti, righti = utils.valueaccumulate(self.value, +1), utils.valueaccumulate(self.value, -1)
+            leftitems = [next(lefti)[1]] + [item for _, item in itertools.takewhile(lambda offset_data: offset_data[0] < threshold, lefti)]
+            rightitems = [next(righti)[1]] + [item for _, item in itertools.takewhile(lambda offset_data: offset_data[0] < threshold, righti)]
+            data = bytes().join([bytes().join(leftitems), bytes().join([item for item in rightitems][::-1])])
+            options['size'] = self.size()
             return u"\"{:s}\"".format(utils.emit_repr(data, threshold, message, **options)) if len(data) > 0 else u"???"
         return u"???"
 

lib/ptypes/utils.py
@@ -249,13 +249,13 @@ def printable(data, nonprintable=u'.'):
     """Return a string of only printable characters"""
     return functools.reduce(lambda agg, item: agg + (bytearray([item]).decode(sys.getdefaultencoding() if hasattr(sys, 'getdefaultencoding') else 'latin1') if item >= 0x20 and item < 0x7f else nonprintable), bytearray(data), u'')
 
-def hexrow(value, offset=0, width=16, breaks=[8]):
+def hexrow(value, offset=0, width=16, breaks=[8], offset_width=4):
     """Returns ``value as a formatted hexadecimal str"""
     value = bytearray(islice(value, width))
     extra = width - len(value)
 
     ## left
-    left = "{:04x}".format(offset)
+    left = "{:0{:d}x}".format(offset, offset_width)
 
     ## middle
     res = [ "{:02x}".format(item) for item in value ]
@@ -282,6 +282,7 @@ def hexdump(value, offset=0, width=16, rows=None, **kwds):
     """
 
     rows = kwds.pop('rows', kwds.pop('lines', None))
+    kwds.setdefault('offset_width', len("{:x}".format(offset + len(value))))
     value = iter(value)
 
     getRow = lambda o: hexrow(data, offset=o, **kwds)
@@ -299,7 +300,7 @@ def hexdump(value, offset=0, width=16, rows=None, **kwds):
         res.append( getRow(ofs) )
     return '\n'.join(res)
 
-def emit_repr(data, width=0, message=' .. skipped {leftover} chars .. ', padding=' ', **formats):
+def emit_repr(data, width=0, message=' ... total {size} bytes ... ', padding=' ', **formats):
     """Return a string replaced with ``message`` if larger than ``width``
 
     Message can contain the following format operands:
@@ -307,28 +308,25 @@ def emit_repr(data, width=0, message=' .. skipped {leftover} chars .. ', padding
     charwidth = width of each character
     bytewidth = number of bytes that can fit within width
     length = number of bytes displayed
-    leftover = approximate number of bytes skipped
     **format = extra format specifiers for message
     """
-    size = len(data)
-    charwidth = len(r'\xFF')
-    bytewidth = width // charwidth
-    leftover = size - bytewidth
+    size = formats.setdefault('size', len(data))
+    charwidth = formats.setdefault('charwidth', len(r'\xFF'))
+    bytewidth = formats.setdefault('bytewidth', width // charwidth)
 
     hexify = lambda data: str().join(map(r"\x{:02x}".format, bytearray(data)))
 
     if width <= 0 or bytewidth >= len(data):
         return hexify(data)
 
-    # FIXME: the skipped/leftover bytes are being calculated incorrectly..
-    msg = message.format(size=size, charwidth=charwidth, width=width, leftover=leftover, **formats)
-
     # figure out how many bytes we can print
-    bytefrac, bytewidth = math.modf((width - len(msg)) * 1.0 / charwidth)
+    formatted = message.format(**formats)
+    bytefrac, bytewidth = math.modf((width - len(formatted)) * 1.0 / charwidth)
     padlength = math.trunc(charwidth * bytefrac)
 
-    msg = padding * math.trunc(padlength / 2.0 + 0.5) + msg + padding * math.trunc(padlength / 2)
-    left, right = data[:math.trunc(bytewidth / 2 + 0.5)], data[size - math.trunc(bytewidth / 2):]
+    # build the string that we'll slice our message into
+    msg = padding * math.trunc(padlength / 2.0 + 0.5) + formatted + padding * math.trunc(padlength / 2)
+    left, right = data[:math.trunc(bytewidth / 2 + 0.5)], data[len(data) - math.trunc(bytewidth / 2):]
     return hexify(left) + msg + hexify(right)
 
 def emit_hexrows(data, height, message, offset=0, width=16, **attrs):
@@ -346,6 +344,7 @@ def emit_hexrows(data, height, message, offset=0, width=16, **attrs):
     half = math.trunc(height / 2.0)
     leftover = (count - half * 2)
     skipped = leftover * width
+    attrs.setdefault('offset_width', len("{:x}".format(offset + size)))
 
     # display everything
     if height <= 0 or leftover <= 0:
@@ -362,6 +361,29 @@ def emit_hexrows(data, height, message, offset=0, width=16, **attrs):
     o2 = width * (count - half)
     for o in range(0, half * width, width):
         yield hexrow(data[o + o2 : o + o2 + width], o + o1 + o2, **attrs)
+    return
+
+def valueiterator(value, direction=+1):
+    '''iterates through all the values in the speciied direction yielding the bytes and their size (not in that order).'''
+    for item in value[::direction]:
+        if isinstance(item.value, bytes):
+            yield len(item.value), item.value
+
+        elif not hasattr(item, 'blockbits'):
+            for size, value in valueiterator(item.value, direction):
+                yield size, value
+            continue
+
+        else:
+            yield item.size(), item.serialize()
+        continue
+    return
+
+def valueaccumulate(value, direction, offset=0):
+    '''iterate through a list yielding the current offset and the bytes that got us there'''
+    for size, value in valueiterator(value, direction):
+        yield offset + size, value
+        offset += size
     return
 
 def attributes(instance):

lib/ptypes/config.py
@@ -344,10 +344,10 @@ defaults.display.show_module_name = False
 defaults.display.show_parent_name = False
 defaults.display.hexdump.width = 16
 defaults.display.hexdump.threshold = 8
-defaults.display.threshold.summary = 80
+defaults.display.threshold.summary = 132
 defaults.display.threshold.details = 8
-defaults.display.threshold.summary_message = ' ..skipped ~{leftover} bytes.. '
-defaults.display.threshold.details_message = ' ..skipped {leftover} rows, {skipped} bytes.. '
+defaults.display.threshold.summary_message = ' ...total {size} bytes... '
+defaults.display.threshold.details_message = ' ...skipped {leftover} rows, total {size} bytes.. '
 defaults.display.mangle_with_attributes = False
 
 # array types

commit eb565f32dabd7b6737a2479f302940c041287eec

Fixed a bug introduced into ptypes by commit 750e59514818ee265101f1f820fe7979aec28622 when summarizing a partially initialized `ptype.container`.

lib/ptypes/pint.py
@@ -315,6 +315,8 @@ class type(ptype.type):
         return super(type, self).__setvalue__(bytes(bytearray(ordered)), **attrs)
 
     def int(self):
+        if self.value is None:
+            return 0
         return self.__getvalue__()
     get = int
 

lib/ptypes/pstr.py
@@ -164,6 +164,8 @@ class _char_t(pint.type):
         return res if len(res) else data
 
     def int(self):
+        if self.value is None:
+            return 0
         return super(_char_t, self).__getvalue__()
 
     def summary(self, **options):
@@ -423,6 +425,9 @@ class string(ptype.type):
         try:
             res = self.__getvalue__()
 
+        except ptypes.error.InitializationError:
+            return super(string, self).summary(**options)
+
         except UnicodeDecodeError:
             Log.debug('{:s}.summary : {:s} : Unable to decode unicode string. Rendering as hexdump instead.'.format(self.classname(), self.instance()))
             return super(string, self).summary(**options)
@@ -540,6 +545,9 @@ class szstring(string):
     def summary(self, **options):
         try:
             res = self.__getvalue__()
+
+        except ptypes.error.InitializationError:
+            return super(szstring, self).summary(**options)
 
         except UnicodeDecodeError:
             Log.debug('{:s}.summary : {:s} : Unable to decode unicode string. Rendering as hexdump instead.'.format(self.classname(), self.instance()))

lib/ptypes/pstruct.py
@@ -362,7 +362,8 @@ class type(__structure_interface__):
                 continue
             offset = self.getoffset(getattr(item, '__name__', None) or name)
             instance = utils.repr_instance(item.classname(), item.name() or name)
-            value = u'???' if item.value is None else item.summary(**options)
+            initialized = item.initializedQ() if isinstance(item, ptype.container) else item.value is not None
+            value = item.summary(**options) if initialized else u'???'
             properties = ','.join(u"{:s}={!r}".format(k, v) for k, v in item.properties().items())
             result.append(u"[{:x}] {:s}{:s} {:s}".format(offset, instance, u" {{{:s}}}".format(properties) if properties else u"", value))
             offset += item.size()

lib/ptypes/ptype.py
@@ -1069,6 +1069,7 @@ class type(base):
         if not self.initializedQ():
             res = self.blocksize()
 
+            # FIXME: this should definitely be clamped to a maximum size
             try:
                 parent = self.getparent(encoded_t)
             except error.ItemNotFoundError:

lib/ptypes/utils.py
@@ -364,10 +364,14 @@ def emit_hexrows(data, height, message, offset=0, width=16, **attrs):
     return
 
 def valueiterator(value, direction=+1):
-    '''iterates through all the values in the speciied direction yielding the bytes and their size (not in that order).'''
+    '''iterates through all the values in the specified direction yielding the bytes and their size (not in that order).'''
     for item in value[::direction]:
         if isinstance(item.value, bytes):
             yield len(item.value), item.value
+
+        # If we encounter None, then the rest of this item should be uninitialized.
+        elif item.value is None:
+            break
 
         elif not hasattr(item, 'blockbits'):
             for size, value in valueiterator(item.value, direction):