cb6ea2853d5fa1c45df1d859eba189dccfcc2a07
Ali Rizvi-Santiago <arizvisa@gmail.com>
2023-11-03 17:51:35 -0500

    Expanded the definition for the lladdr type from the osi.datalink.ethernet template module.

--- template/osi/datalink/ethernet.py
+++ template/osi/datalink/ethernet.py
@@ -8,7 +8,21 @@ pint.setbyteorder(ptypes.config.byteorder.bigendian)
 class u_char(pint.uint8_t): pass
 class u_short(pint.uint16_t): pass
 
-class lladdr(parray.type): length, _object_ = 6, u_char
+class oui(pbinary.struct):
+    _fields_ = [
+        (6, 'sextet'),
+        (1, 'local'),
+        (1, 'multicast'),
+    ]
+
+class lladdr(parray.type):
+    length, _object_ = 6, lambda self: u_char if self.value else oui
+    def summary(self):
+        oui = self[0]
+        scope = 'local' if oui['local'] else 'global'
+        cast = 'multicast' if oui['multicast'] else 'unicast'
+        octets = [ "{:02X}".format(octet.int()) for octet in self ]
+        return "({:s}) {:s}".format(','.join([scope,cast]), '-'.join(octets))
 
 class header(pstruct.type, stackable):
     _fields_ = [
