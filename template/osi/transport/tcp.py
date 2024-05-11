540c7f70923c306576dfac719beb6710c355fdda
Ali Rizvi-Santiago <arizvisa@gmail.com>
2024-05-11 08:44:16 -0500

    Added some more flags to the osi.transport.tcp templates and improved some of the summaries.

--- template/osi/transport/tcp.py
+++ template/osi/transport/tcp.py
@@ -12,22 +12,33 @@ class u_long(pint.uint32_t): pass
 class tcp_seq(pint.uint32_t): pass
 
 class flags(pbinary.flags):
-    _fields_ = [(1,f) for f in ('URG','ACK','PSH','RST','SYN','FIN')]
+    _fields_ = [(1,F) for F in ('NON','CWR','ECE','URG','ACK','PSH','RST','SYN','FIN')]
 
 @network.layer.define
 class header(pstruct.type, stackable):
     type = 0x06
     class _offset_and_flags(pbinary.struct):
+        class _th_off(pbinary.integer):
+            def blockbits(self):
+                return 4
+            def summary(self):
+                res = super(header._offset_and_flags._th_off, self).summary()
+                return "{:s} : {:+d} bytes".format(res, 4 * self.int())
         _fields_ = [
-            (4,'th_off'),
-            (6,'th_rsvd'),
+            (_th_off,'th_off'),
+            (3,'th_rsvd'),
             (flags, 'th_flags')
         ]
         def summary(self):
-            return '{:d} | {:s}'.format(self['th_off'], self['th_flags'].summary())
+            res = []
+            res.append("th_off={:d} ({:+d})".format(self['th_off'], 4 * self['th_off']))
+            res.append("th_rsvd={:s}".format(self.field('th_rsvd').summary())) if self['th_rsvd'] else ()
+            return "{:s} | th_flags={:s}".format(' '.join(res), self['th_flags'].summary())
 
     def __th_options(self):
-        total = self['th_off/th_flags'].li['th_off']*4
+        res = self['off_flags'].li
+        total = 4 * res['th_off']
+        # FIXME: implement these options
         return dyn.array(u_char, total - 0x14)
 
     _fields_ = [
@@ -38,7 +49,7 @@ class header(pstruct.type, stackable):
 
         #(u_char, 'th_off'),
         #(u_char, 'th_flags'),
-        (pbinary.bigendian(_offset_and_flags), 'th_off/th_flags'),
+        (pbinary.bigendian(_offset_and_flags), 'off_flags'),
 
         (u_short, 'th_win'),
         (u_short, 'th_sum'),
@@ -48,7 +59,7 @@ class header(pstruct.type, stackable):
     ]
 
     def th_off(self):
-        return self['th_off/th_flags']['th_off']
+        return self['off_flags']['th_off']
 
     def th_flags(self):
-        return self['th_off/th_flags']['th_flags']
+        return self['off_flags']['th_flags']
