71e8df13eabf56d1f36e0b2d356dc0862dc50843
Ali Rizvi-Santiago <arizvisa@gmail.com>
2025-05-02 17:48:33 -0500

    Removed the imports for some unnecessary protocols from the `protocol` templates.

--- template/protocol/__init__.py
+++ template/protocol/__init__.py
@@ -1,12 +1,11 @@
 from . import ldap
 from . import x224
-#from . import rdesktop
-from . import x509, ssl, tls
+#from . import x509, ssl, tls, tlsext
 from . import aspie, mp, tara
 from . import ber, per
 from . import nlmp, credssp, spnego, spnegoex
 from . import gcc, mcs, mas
 from . import dns, netbios
-from . import kerberos
+#from . import kerberos
 from . import alljoyn
 from . import rfc3208
