3fb58afd438b3716cac2dbaf52893b105c528067
Ali Rizvi-Santiago <arizvisa@gmail.com>
2025-08-07 15:06:28 -0500

    Updated the osi templates to register `datalink.ethernet.ethhdr` in the osi layer types.

--- template/osi/datalink/ethernet.py
+++ template/osi/datalink/ethernet.py
@@ -3,6 +3,7 @@ from ptypes import *
 
 from . import layer, stackable, terminal
 from . import utils, address
+from .. import layer as link
 
 pint.setbyteorder(ptypes.config.byteorder.bigendian)
 
@@ -10,7 +11,9 @@ class u_char(pint.uint8_t): pass
 class u_short(pint.uint16_t): pass
 lladdr = address.ethaddr
 
+@link.define
 class ethhdr(pstruct.type, stackable):
+    type = layer.enum.byname('ETHERNET')
     def __type(self):
         from .. import network
         # FIXME: these enumerations could be better organized.
