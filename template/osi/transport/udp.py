747c9b162a905b27213170201aaab54e1e8dcb6c
Ali Rizvi-Santiago <arizvisa@gmail.com>
2025-08-29 16:11:34 -0500

    Renamed the fields from the `osi.transport.udp` templates to their BSD equivalents.

--- template/osi/transport/udp.py
+++ template/osi/transport/udp.py
@@ -12,15 +12,22 @@ class u_long(pint.uint32_t): pass
 @network.layer.define
 class header(pstruct.type, stackable):
     type = 0x11
+
     _fields_ = [
-        (u_short, 'source port'),
-        (u_short, 'dest port'),
-        (u_short, 'length'),
-        (u_short, 'checksum'),
+        (u_short, 'uh_sport'),  # source port
+        (u_short, 'uh_dport'),  # destination port
+        (u_short, 'uh_ulen'),   # udp length
+        (u_short, 'uh_sum'),    # udp checksum
     ]
 
     def layer(self):
         layer, id, remaining = super(header, self).layer()
-        res, fields = self['length'].li, ['source port', 'dest port', 'length', 'checksum']
+        res, fields = self['uh_ulen'].li, ['uh_sport', 'uh_dport', 'uh_ulen', 'uh_sum']
         length = res.int() if remaining is None else min(remaining, res.int())
         return layer, id, max(0, length - sum(self[fld].li.size() for fld in fields))
+
+    def summary(self):
+        res = []
+        for field in self:
+            res.append("{:s}={:#x}".format(field, self[field]))
+        return ' '.join(res)
